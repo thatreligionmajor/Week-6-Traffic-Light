2b89e0870354ec742b97a0f97a68442241a66e79
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-07-29 02:31:30 +0000

    added functionality on/off

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -8,17 +8,23 @@ const TrafficLight = () => {
             <div className="trafficLightBody">
                 <div 
                 className={chosenColor === "red" ? "light redLight redGlow" : "light redLight"}
-                onClick={() => setChosenColor("red")}
+                onClick={
+                    chosenColor === "red" ? () => 
+                    setChosenColor("") : () => setChosenColor("red")}
                 ></div>
 
                 <div 
                 className={chosenColor === "yellow" ? "light yellowLight yellowGlow" : "light yellowLight"}
-                onClick={() => setChosenColor("yellow")}
+                onClick={
+                    chosenColor === "yellow" ? () => 
+                    setChosenColor("") : () => setChosenColor("yellow")}
                 ></div>
 
                 <div 
                 className={chosenColor === "green" ? "light greenLight greenGlow" : "light greenLight"}
-                onClick={() => setChosenColor("green")}
+                onClick={
+                    chosenColor === "green" ? () => 
+                    setChosenColor("") : () => setChosenColor("green")}
                 ></div>
 
                 <p class="text-muted text-center">chi va piano va sano e va lontano, chi va forte va incontro alla morte</p>
