7710e1ef754f078050aa083a499d50b2d23ab5a3
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-07-29 08:59:29 -0400

    Update trafficLight.jsx

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -9,22 +9,20 @@ const TrafficLight = () => {
                 <div 
                 className={chosenColor === "red" ? "light redLight redGlow" : "light redLight"}
                 onClick={
-                    chosenColor === "red" ? () => 
-                    setChosenColor("") : () => setChosenColor("red")}
+                    chosenColor === "red" ? 
+                    () => setChosenColor("") : () => setChosenColor("red")}
                 ></div>
 
                 <div 
                 className={chosenColor === "yellow" ? "light yellowLight yellowGlow" : "light yellowLight"}
                 onClick={
-                    chosenColor === "yellow" ? () => 
-                    setChosenColor("") : () => setChosenColor("yellow")}
+                    chosenColor === "yellow" ? () => setChosenColor("") : () => setChosenColor("yellow")}
                 ></div>
 
                 <div 
                 className={chosenColor === "green" ? "light greenLight greenGlow" : "light greenLight"}
                 onClick={
-                    chosenColor === "green" ? () => 
-                    setChosenColor("") : () => setChosenColor("green")}
+                    chosenColor === "green" ? () => setChosenColor("") : () => setChosenColor("green")}
                 ></div>
 
                 <p class="text-muted text-center">chi va piano va sano e va lontano, chi va forte va incontro alla morte</p>
